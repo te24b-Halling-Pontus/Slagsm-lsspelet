836c0ab7ac17188cad5a02c36e94e68006132b6b
Pontus Halling <pontushalling08@gmail.com>
2025-11-13 18:07:23 +0100

    Jag märkte att jag inte comitade på min gamla dator så håller på att göra om alting nu.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -31,19 +31,37 @@ static void start(string p1, string p2)
 {
     int p1hp = 100;
     int p2hp = 100;
+    int roundCounter = 1;
+    int dmgOnP2;
+    int dmgOnP1;
 
     Console.WriteLine($"Den store starka {p1} möter den coola snabba {p2}");
-    while (p1hp <= 0 && p2hp <= 0)
+    while (p1hp >= 0 && p2hp >= 0)
     {
-       
+        
+        dmgOnP2 = damage();
+        dmgOnP1 = damage();
+        if (dmgOnP2 > p2hp)
+        {
+        dmgOnP2 = p1hp;  
+        Console.WriteLine($"\n\n{p1} slog på {p2} och gjorde {dmgOnP2} i skada och i ju med det solg ut {p2} och van");
+        }
+        p2hp -= dmgOnP2; 
+        Console.WriteLine($"\n\n{p1} slog på {p2} och gjorde {dmgOnP2} i skada, nu har {p2} {p2hp}HP kvar");
+        p1hp -= dmgOnP1;
+        Console.WriteLine($"\n{p2} slog på {p1} och gjorde {dmgOnP2} i skada, nu har {p1} {p1hp}HP kvar");
+        roundCounter++;
+        Console.WriteLine($"\nRunda {roundCounter}");
+        Console.ReadLine();
     }
 
 }
 
 
-static void damage()
+static int damage()
 {
-    int dmg = Random.Shared.Next(0, 20);
-    
+    int dmg = Random.Shared.Next(21);
+    return dmg;
 }
  
+Console.ReadLine();
